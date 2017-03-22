74cd9294318f219bff24e5ec72ab7b43aeb94828
Gardakan93 <delguste.tom@gmail.com>
2017-03-22 15:58:47 +0100

    ScreenShotDAUNE

---
+++ Assets/Scripts/FileFinder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FileFinder : MonoBehaviour
+{
+
+    public Button button;
+    public Text filePathText;
+    private string path;
+    private static string NOFILESTRING = "No file selected.";
+    void Start()
+    {
+        button.onClick.AddListener(filePanelOnClick);
+        Debug.Log("Path button launched...");
+        filePathText.text = NOFILESTRING;
+    }
+
+    void filePanelOnClick()
+    {
+        Debug.Log("clicked");
+        this.path = EditorUtility.OpenFilePanel("Select a file","","fbx");
+        if (path.Length == 0)
+            this.path = NOFILESTRING;
+        filePathText.text = this.path;
+        Debug.Log("Link is :" + this.path);
+    }
+
+    string getPath()
+    {
+        if (this.path.Length == 0)
+            throw new NoFileException();
+        return this.path;
+    }
+}
+
+
+internal class NoFileException : Exception
+{
+    public NoFileException()
+    {
+    }
+
+    public NoFileException(string message) : base(message)
+    {
+    }
+
+    public NoFileException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    protected NoFileException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
---
+++ Assets/Scripts/SpriteMaker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpriteMaker : MonoBehaviour {
+	
+	
+	
+	public void CaptureScreenshots(){
+		int count=0;
+		string path="";
+
+		
+		if(path.Equals("")){
+			path = EditorUtility.SaveFilePanel("Save_Screenshots_Folder",Application.dataPath,"","");
+			Debug.Log(path);
+		}
+
+
+		
+		Debug.Log("Taking picture");
+		
+
+		Texture2D texture = new Texture2D(Screen.width,Screen.height,TextureFormat.ARGB32,false);
+		texture.ReadPixels(new Rect(0,0, Screen.width, Screen.height),0,0);
+		texture.Apply();
+
+		byte[] bytes = texture.EncodeToPNG();
+		File.WriteAllBytes(path+"screen_"+ count + ".png",bytes);
+		count ++;
+		DestroyObject(texture);
+	
+	}
+}
