02f08bcaec33d149f6d466b90538ed0aa6f57a1b
Lorenso Mendes <mendeslorenso@hotmail.fr>
2017-03-27 12:26:22 +0200

    Liste des Scripts de déformation

---
+++ BibliothequeTexture/BibliothequeDeformation.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BibliothequeDeformation : MonoBehaviour {
+
+	public GameObject scrollview;
+	public string pathToMaterials;
+	public Deformation3D script;
+	private Component contentElement;
+
+	// Use this for initialization
+	void Start () {
+		//Recherche de l'endroit où placer les elements sur l'UI
+		foreach (Component go in this.GetComponentsInChildren<Component>()) {
+			if (go.name == "Content") {
+				this.contentElement = go;
+				break;
+			}
+		}
+
+		//Get l'arboressance du dossier
+		FileTree mainTree = new FileTree (pathToMaterials.TrimStart(pathToMaterials.ToCharArray()), pathToMaterials);
+		List<string> listScriptPath = mainTree.getPaths();
+		for(int i = 0; i<listScriptPath.Count; i++) {
+			if (!listScriptPath[i].EndsWith (".cs")) {
+				listScriptPath.RemoveAt(i);
+				i--;
+			}
+		}
+
+		float y = -20;
+		string[] CategorieTexture = {};
+		int tab=0;
+
+		foreach (string path in listScriptPath) {
+			int j = 0;
+			string[] newCat = path.TrimStart ((pathToMaterials + "\\").ToCharArray ()).Split ("\\".ToCharArray(),200);
+			try{
+				while(CategorieTexture[j] == newCat[j]){
+				j++;
+				}
+			}catch(System.IndexOutOfRangeException){
+
+			}
+			CategorieTexture = newCat;
+			for (int i = j ; i < CategorieTexture.Length - 1; i++) {
+				tab = i * 20 + 20; //px 
+				Text typeElem = creerText ("type",
+					                CategorieTexture [i],
+					                new Vector3 (tab, y, 0),
+					                new Vector2 (290, 20),
+					                contentElement,
+					                FontStyle.BoldAndItalic);
+				y -= typeElem.rectTransform.sizeDelta.y;
+
+
+			}
+			
+			
+			Text textElem = creerText("script",
+				newCat[newCat.Length-1].TrimEnd(".cs".ToCharArray()),
+				new Vector3 (tab+20, y, 0),
+				new Vector2 (290, 20),
+				contentElement,
+				FontStyle.Normal);
+			y -= textElem.rectTransform.sizeDelta.y;
+			//Creation du clickable 
+			textElem.gameObject.AddComponent(typeof(Button));
+			textElem.GetComponent<Button>().onClick.AddListener(delegate{
+				script.deformer();
+			});
+	
+		}
+	
+	}
+
+	Text creerText(string nom, string text, Vector3 pos,  Vector2 size, Component parent, FontStyle fontStyle ){
+		GameObject go = new GameObject ();
+		Text UIText = go.AddComponent<Text> ();
+		UIText.name=nom;
+
+		//Text
+		UIText.text = text;
+		UIText.font = Resources.GetBuiltinResource (typeof(Font), "Arial.ttf") as Font;
+		UIText.fontStyle = fontStyle;
+		//Parent
+		UIText.transform.SetParent (parent.gameObject.transform);
+
+		//Position
+		UIText.transform.localPosition = pos;
+		UIText.rectTransform.sizeDelta = size;
+		return UIText;
+	}
+			
+	static bool comparer(string[] s1, string[] s2){
+		if (s1.Length != s2.Length)
+			return false;
+		for (int i = 0; i < s1.Length-1; i++) {
+			if (s1 [i] != s2 [i])
+				return false;
+		}
+		return true;
+	}
+
+	static string afficherContenu(string[] list){
+		string ret = "{";
+		foreach(string st in list){
+			ret += st+",";
+		}
+		return ret + "}";
+	}
+}
---
+++ BibliothequeTexture/FileTree.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class FileTree {
+
+	public readonly string name;
+	public readonly List<string> files;
+	public readonly List<FileTree> directory;
+
+	public FileTree(string name,string path){
+		this.name = name;
+		files = new List<string> ();
+		directory = new List<FileTree>();
+		this.lookForFiles(path);
+	}
+
+	public void lookForFiles(string path){
+		files.AddRange(Directory.GetFiles(path));
+	
+		foreach (string dir in Directory.GetDirectories(path)) {
+			this.directory.Add (new FileTree(dir,dir));
+		}
+	}
+
+	public List<string> getPaths(){
+		List<string> list = new List<string>();
+		list.AddRange(files);
+		foreach (FileTree ft in directory) {
+			list.AddRange(ft.getPaths());
+		}
+		return list;
+	}
+
+	public string ToString(){
+		string s = "\n"+this.name;
+		foreach (string file in files)
+			s += "\n file:" + file;
+		foreach (FileTree ft in directory)
+			s += ft.ToString();
+		return s+"\n";
+	}
+
+}
