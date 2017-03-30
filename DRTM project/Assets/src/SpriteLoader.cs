e265e02286d1130b8b5e8d5aee5bc6071713452e
ozidrice <trolarddamien@gmail.com>
2017-03-30 14:22:50 +0200

    Version Finale 30/03/17

---
+++ DRTM project/Assets/src/SpriteLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+﻿using System;
+public class SpriteLoader : MonoBehaviour {
+
+
+	
+	
+
+
+
+   	 public void onClick() {
+
+		 string path = EditorUtility.OpenFilePanel("Select an image","/Assets/Screenshots","png");
+		 Application.OpenURL(path);
+   	 }
+	
+
+
+
+
+
+	
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# ProjetDRTM
+
+Projet de fin de DUT Informatique
+
+"Génération aléatoire de texture et transformation irréaliste" 
+
+Rdv dans la partie "Documents" pour plus d'informations
